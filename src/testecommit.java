12cdee3cc2cd45ed1dc8d2df706ffac3082b51e0
Wesley <wesleysales.vb@gmail.com>
2024-04-29 23:57:51 -0300

    primeiro projeto, calculo da idade

---
+++ src/testecommit.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class testecommit {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int idade, anoNascimento,anoAtual;
+
+        System.out.println("Digite o ano atual");
+        anoAtual = teclado.nextInt();
+        System.out.println("Digite o ano do seu nascimento");
+        anoNascimento = teclado.nextInt();
+
+        idade = anoAtual - anoNascimento;
+
+        System.out.println(idade);
+    }
+}
